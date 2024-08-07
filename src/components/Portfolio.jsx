29557b65efa3be2bb3e15df4da545af41e4b01ec
Bello Israel <belloisrael1999@gmail.com>
2024-08-07 11:19:14 -0700

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,42 +12,36 @@ const portfolios = [
     src: pic1,
     demoLink: "https://hotel-ng-two.vercel.app",
     codeLink: "https://github.com/Israelite10/hotel.ng.git",
-    description: "Hotel.ng landing page",
   },
   {
     id: 2,
     src: pic2,
     demoLink: "https://group11-3rdproject-inkeeper.vercel.app",
     codeLink: "https://github.com/Israelite10/group11-3rdproject-inkeeper.git",
-    description: "Digiplus landing page",
   },
   {
     id: 3,
     src: pic3,
     demoLink: "https://streamflix-self.vercel.app",
     codeLink: "https://github.com/Adeola-oluwa/FWD-LSETF-Netclone.git",
-    description: "Netflix app clone",
   },
   {
     id: 4,
     src: pic4,
     demoLink: "https://weather-app-seven-mu-50.vercel.app",
     codeLink: "https://github.com/sarahannie/weather_app.git",
-    description: "Weather App",
   },
   {
     id: 5,
     src: pic5,
     demoLink: "https://calculator-codsoft-theta.vercel.app/",
     codeLink: "https://github.com/Israelite10/Calculator-Codsoft.git",
-    description: "Calculator App",
   },
   {
     id: 6,
     src: pic6,
     demoLink: "https://timbu-app-hng-stage2.vercel.app/",
     codeLink: "https://github.com/Israelite10/Timbu-app-Hng-stage2.git",
-    description: "Timbu shop cart app",
   },
 ];
 
@@ -74,11 +68,10 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, demoLink, codeLink, description }) => (
+          {portfolios.map(({ id, src, demoLink, codeLink }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
-                alt={description}
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex flex-col items-center justify-center p-4">
@@ -96,7 +89,6 @@ const Portfolio = () => {
                     Code
                   </button>
                 </div>
-                <p className="text-center mt-2">{description}</p>
               </div>
             </div>
           ))}
