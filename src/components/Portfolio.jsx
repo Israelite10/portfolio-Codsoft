8dd39a59e68bce2b00cf8c6523db8e91ca1dc804
Bello Israel <belloisrael1999@gmail.com>
2024-08-08 05:56:42 -0700

    done

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -59,7 +59,7 @@ const Portfolio = () => {
       name="portfolio"
       className="bg-gradient-to-b  from-black to-gray-800 w-full  text-white  md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center pt:0 lg:pt-10 pb:0 lg:pb-10 w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
