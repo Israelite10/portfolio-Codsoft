c2dc88e74a492c70b9464fd237d5da6a9ded422c
Bello Israel <belloisrael1999@gmail.com>
2024-08-08 04:36:21 -0700

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      {/* <p className=" text-center">Copyright ©2024; Designed by ISRAEL</p> */}
+      <p className=" text-center">Copyright ©2024; Designed by ISRAEL</p>
     </div>
   );
 };
