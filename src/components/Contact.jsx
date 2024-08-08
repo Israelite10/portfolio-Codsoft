f7ac1702b6df5ed9e2e6e5cf16c859a8988bafb2
Bello Israel <belloisrael1999@gmail.com>
2024-08-08 03:55:56 -0700

    padding fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-500 pt-10  text-white p-4"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-500  text-white p-4"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
