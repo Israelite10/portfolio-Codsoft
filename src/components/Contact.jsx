f312ebc0192dc0b2e507c749eb55579a3dcab211
Bello Israel <belloisrael1999@gmail.com>
2024-08-08 03:51:47 -0700

    fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,10 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-500   text-white p-4"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-500 pt-10  text-white p-4"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-6">
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
